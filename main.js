284b6d67c5c1f3eb755a86f5a43f6d2ba8e04689
Dennis Empirio <dennis.empirio@gmail.com>
2022-11-23 15:55:39 +0100

    js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const posts = [
     {
         "id": 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad   ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/300?image=171",
         "author": {
             "name": "Phil Mangione",
@@ -55,3 +55,63 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+// Milestone 1 -
+// Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card: - id del post, numero progressivo da 1 a n - nome autore, - foto autore, - data in formato americano (mm-gg-yyyy), - testo del post, - immagine (non tutti i post devono avere una immagine), - numero di likes. Non è necessario creare date casualiPer le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+// [Qui come abbiam visto in realtà vi arriva gratis, ma dategli un occhio per capirlo e vedete se tutto combacia e torna :innocente:]
+// Milestone 2 -
+// Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+// Milestone 3 -
+// Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+// Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+
+const PostContainer = document.getElementById('container');
+
+for (let i = 0; i < posts.length; i++) {
+   
+    let post = 
+
+        `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${posts[i].image}" alt="">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${posts[i].name}</div>
+                    <div class="post-meta__time">${posts[i].created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${posts[i].content}</div>
+        <div class="post__image">
+            <img src="${posts[i].media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+        </div>`
+    
+
+    PostContainer.innerHTML += post;
+}
+
+
+
+
+
+
+
+
+
